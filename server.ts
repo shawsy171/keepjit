1b156d38084712a6d2859a5d18dbe6d54f66a8ee
Damien Shaw <damien.shaw@hotmail.co.uk>
2019-02-09 21:55:12 +0000

    feat: access origin update

--- server.ts
+++ server.ts
@@ -20,6 +20,11 @@ app.prepare()
       extended: true
     }));
 
+    server.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
     server.use(bodyParser.json());
 
     server.use('/api', apiRouter);
